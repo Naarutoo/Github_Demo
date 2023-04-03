33abb36ef25c4ed838f2d48a31bd0ebd03214f53
Saurabh <saurabhsharma708@gmail.com>
2023-04-03 16:48:05 +0530

    This is my second commit

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.githubdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getData()
+
+    }
+
+    fun getData() {
+        Toast.makeText(this, "my name is saurabh", Toast.LENGTH_SHORT).show()
     }
 }
