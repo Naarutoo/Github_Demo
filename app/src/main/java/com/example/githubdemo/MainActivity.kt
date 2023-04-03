b41a8657913b6b2f9fc59020ef6c673107c99a6d
Saurabh <saurabhsharma708@gmail.com>
2023-04-03 16:51:47 +0530

    This is my Third commit

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -9,10 +9,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getData()
-
+        makeApi()
     }
 
     fun getData() {
         Toast.makeText(this, "my name is saurabh", Toast.LENGTH_SHORT).show()
+    }
+
+    fun makeApi() {
+        Toast.makeText(this, "this is api calling function", Toast.LENGTH_LONG).show()
     }
 }
